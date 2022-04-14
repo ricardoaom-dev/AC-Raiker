fe120094c3b2845040e6cb09b271278dbba87506
Riccardo Ordoñez <ricardoaom.dev@gmail.com>
2022-04-14 16:37:47 -0500

    Absttract LocationDataSource to meet DIP

--- app/src/main/java/com/raikerxv/model/RegionRepository.kt
+++ app/src/main/java/com/raikerxv/model/RegionRepository.kt
@@ -1,13 +1,9 @@
 package com.raikerxv.model
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.location.Geocoder
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.location.LocationServices
-import kotlin.coroutines.resume
-import kotlinx.coroutines.suspendCancellableCoroutine
 
 class RegionRepository(activity: AppCompatActivity) {
 
@@ -15,7 +11,7 @@ class RegionRepository(activity: AppCompatActivity) {
         private const val DEFAULT_REGION = "US"
     }
 
-    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+    private val locationDataSource = PlayServicesLocationDataSource(activity)
     private val coarsePermissionChecker = PermissionChecker(
         activity,
         Manifest.permission.ACCESS_COARSE_LOCATION
@@ -26,17 +22,8 @@ class RegionRepository(activity: AppCompatActivity) {
 
     private suspend fun findLastLocation(): Location? {
         val success = coarsePermissionChecker.request()
-        return if (success) lastLocationSusupended() else null
+        return if (success) locationDataSource.findLastLocation() else null
     }
-
-    @SuppressLint("MissingPermission")
-    private suspend fun lastLocationSusupended(): Location? =
-        suspendCancellableCoroutine { continuation ->
-            fusedLocationClient.lastLocation
-                .addOnCompleteListener {
-                    continuation.resume(it.result)
-                }
-        }
 
     private fun Location?.toRegion(): String {
         val addresses = this?.let {
